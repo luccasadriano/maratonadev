ba98d5b6470c8638927160d3c953a16be939d143
luccasadriano <luccas-adriano@hotmail.com>
2020-02-18 00:30:04 -0300

    Primeiro comite

---
+++ frontend/scripts.js
@@ -0,0 +1,3 @@
+document.querySelector('header button').addEventListener("click", function() {
+    document.querySelector('.form').classList.toggle('hide')/* toggle verificar se o "form" tem a "hide", se estiver retira, se não tem coloca */
+})
